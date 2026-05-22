585bbf372a9e9bbc47fb3e7a03aa75d12e1d9586
Griphcode <148160412+GriphcodeDev@users.noreply.github.com>
2026-05-22 15:15:42 +0200

    commit (4)

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,24 @@
+const { feedPlugin } = require("@11ty/eleventy-plugin-rss");
+
+
+
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(feedPlugin, {
+        type: "atom",
+        outputPath: "/rss.xml",
+        collection: {
+            name: "posts",
+            limit: 0,
+
+        },
+        metadata: {
+            title: "Griphcode | Blog",
+            base: "https://blog.griphcode.dev",
+            subtitle: "My person blog :)",
+            author: {
+                name: "griphcode",
+            }
+        }
+    });
     eleventyConfig.addPassthroughCopy("assets");
 }
